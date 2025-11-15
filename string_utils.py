5f8b4957130a7426e56faf169ba8a8592ad0178f
shirblum1 <shirblum1@gmail.com>
2025-11-15 20:01:32 +0200

    Update string_utils.pyשתיהן

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,16 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+  start = 0
+  end = 1
+  split_formula = [] 
+
+  for char in formula[1:]:
+    if char.isupper():
+      split_formula.append(formula[start:end])
+      start = end
+    end += 1
+
+  split_formula.append(formula[start:end])
+  return split_formula
 
 
 def split_at_first_digit(formula):
