93bd788cee8406f4fe1c2d0192e368abad8694ec
shirblum1 <shirblum1@gmail.com>
2025-11-15 20:21:54 +0200

    Update string_utils.pyסופי אמן אמן

--- string_utils.py
+++ string_utils.py
@@ -14,18 +14,20 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-  digit_location = 1
-  for char in formula[1:]:
-    if char.isdigit():
-      break
-    digit_location += 1
-  if digit_location == len(formula): 
-   return (formula, 1)
+    digit_location = 1
 
-  prefix = formula[:digit_location]
-  number = int(formula[digit_location:])
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
 
-  return (prefix, number)
+    if digit_location == len(formula):
+        return (formula, 1)
+
+    prefix = formula[:digit_location]
+    number = int(formula[digit_location:])
+
+    return (prefix, number)
 
 
    
