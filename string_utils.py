a1ca03d38e43b0b48a26dbeb40f78e99d5c3eebd
shirblum1 <shirblum1@gmail.com>
2025-11-15 20:14:58 +0200

    Update string_utils.pyסופי הלוואי

--- string_utils.py
+++ string_utils.py
@@ -1,16 +1,16 @@
 def split_before_each_uppercases(formula):
-  start = 0
-  end = 1
-  split_formula = [] 
+    start = 0
+    end = 1
+    split_formula = []
 
-  for char in formula[1:]:
-    if char.isupper():
-      split_formula.append(formula[start:end])
-      start = end
-    end += 1
+    for char in formula[1:]:
+        if char.isupper():
+            split_formula.append(formula[start:end])
+            start = end
+        end += 1
 
-  split_formula.append(formula[start:end])
-  return split_formula
+    split_formula.append(formula[start:end])
+    return split_formula
 
 
 def split_at_first_digit(formula):
